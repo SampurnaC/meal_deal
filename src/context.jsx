09f56e45d52da0cf8589b2d1be4be60b083db544
sam <sampurna.chapagainnn@gmail.com>
2022-11-09 22:51:27 +0000

    work on search component and refactor code

--- src/context.jsx
+++ src/context.jsx
@@ -4,14 +4,16 @@ import { useEffect, useState } from 'react';
 
 const AppContext = React.createContext();
 
-const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a';
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 const AppProvider = ({ children }) => {
   const [meals, setMeals] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const fetchMeals = async (url) => {
+    console.log(url);
     setLoading(true);
     try {
       const response = await axios(url);
@@ -27,10 +29,10 @@ const AppProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    fetchMeals(allMealsUrl);
-  }, []);
+    fetchMeals(`${allMealsUrl}${searchTerm}`);
+  }, [searchTerm]);
   return (
-    <AppContext.Provider value={{ loading, meals }}>
+    <AppContext.Provider value={{ loading, meals, setSearchTerm }}>
       {children}
     </AppContext.Provider>
   );
